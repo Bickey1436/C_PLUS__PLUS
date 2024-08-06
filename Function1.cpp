351525bd29308cba9c4a67621b8816d9776eb10f
Bickey shah <50171524+Bickey1436@users.noreply.github.com>
2024-08-06 12:39:30 +0545

    Add files via upload

--- Function1.cpp
+++ Function1.cpp
@@ -1,1 +1,47 @@
 #include<iostream>
+using namespace std;
+
+//datatype
+//function decleration
+//function defination
+//call or invoke functions
+
+//functions increases readability
+//It organise our code
+//it helps in reusability
+//
+
+
+//datatype //function decleration//function definition
+
+
+void Helloworld(){
+
+cout<<"hello world";
+
+}
+
+void multiply(int a, int b){ //// a and b are parameters.
+
+
+
+cout<<a*b<<endl;
+
+}
+
+
+
+int main(){
+    
+    
+    
+Helloworld(); //call or invoke function
+
+int a,b;
+cin>>a>>b;
+multiply(a,b);  //a and b are arguments
+
+
+
+
+}
