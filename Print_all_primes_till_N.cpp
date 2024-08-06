e76c6135cc8078ac7221cb0fc8103d1986159549
Bickey shah <50171524+Bickey1436@users.noreply.github.com>
2024-08-06 22:15:50 +0545

    Add files via upload

---
+++ Print_all_primes_till_N.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+
+void prime(int n){
+
+
+for(int i=2; i<=n;i++){
+
+    if(i%2!=0){
+
+            cout<<i<<endl;
+
+
+    }
+
+
+}
+
+}
+
+int main(){
+
+
+int n;
+cin>>n;
+
+prime(n);
+
+
+
+
+}
