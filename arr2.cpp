7656fb1211c358f682f1b15fde821b79b80fd8b1
Bickey shah <50171524+Bickey1436@users.noreply.github.com>
2024-08-17 15:59:43 +0545

    Add files via upload

---
+++ arr2.cpp
@@ -0,0 +1,27 @@
+//Different way to create array
+//initalization
+//User Input
+
+int arr[5]={1,2,3,4,5}; //Initalization first way.
+int a[100]={1,2,3};//second way of initalization.
+int a[100]={0};//all container aligned 0; true only for 0 case;
+int a[100]={1}; //only oth index will have value 1 and rest 0;
+int a[]={1,3,4,5,6} //Bydefault the size will be 5;
+
+// we not skip the size while initalisizing like a[100]; we can do a[] only when declearing the elements of particular arr like a[]={1,2,3,4};
+
+//brute force, cout<<a[1]<<a[2] and soon;
+
+//using for loop to cout the a[i];
+
+
+int n;
+cin>>n;
+
+int a[n];
+
+for(int 1=0;i<n;i++){
+
+	cin>>a[i];
+}
+
